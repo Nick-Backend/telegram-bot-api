178492e320f6c4a5bd02c9147b45e631b1c101d3
djumanov <djumanovdev@gmail.com>
2026-02-03 19:55:38 +0500

    get updates and send message

--- main.py
+++ main.py
@@ -5,12 +5,37 @@ from config import TOKEN
 
 BASE_URL = F'https://api.telegram.org/bot{TOKEN}'
 
+
 def get_me() -> dict:
-    get_url = f'{BASE_URL}/getMe'
+    get_me_url = f'{BASE_URL}/getMe'
     
-    response = requests.get(get_url)
+    response = requests.get(get_me_url)
 
     if response.status_code == 200:
         return response.json()
     else:
         raise Exception('Telegram server xatolik qaytardi!')
+
+
+def get_updates() -> list[dict]:
+    get_updates_url = f'{BASE_URL}/getUpdates'
+    
+    response = requests.get(get_updates_url)
+
+    if response.status_code == 200:
+        return response.json()['result']
+    else:
+        raise Exception('Telegram server xatolik qaytardi!')
+    
+
+def send_message() -> None:
+    send_message_url = f'{BASE_URL}/sendMessage'
+    
+    params = {
+        'chat_id': 1258594598,
+        'text': 'Hello World'
+    }
+    requests.get(send_message_url, params=params)
+
+
+send_message()
